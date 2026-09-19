f99cf32007c5229587ab3dbfccb38ef77dd22c3a
Panda_JS <74700129+fruit8067@users.noreply.github.com>
2026-09-19 22:53:20 +0900

    B: AC

---
+++ ABC476/B.py
@@ -0,0 +1,11 @@
+N = int(input())
+S = input()
+T = input()
+for i in range(N):
+    if T[i] == '*':
+        continue
+    if T[i] != S[i]:
+        print("No")
+        break
+else:
+    print("Yes")
